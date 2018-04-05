39d3d7cd88ea69bb872b8513d60b9272b758c6af
Patrick Fief <froststang@gmail.com>
2018-04-05 17:52:13 -0500

    Playing with FX

---
+++ Testing/LoginFX/LoginFXML.java
@@ -0,0 +1,76 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.paint.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+ 
+public class LoginFXML extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+        
+        //Creating the root node as a GridPane
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        //Creating Controls
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setId("welcome-text");
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+        
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+        
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+        
+        //Displays the grid lines for debugging
+        //grid.setGridLinesVisible(true);
+        
+        //Event Handling
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setId("actiontarget");
+                actiontarget.setText("Sign in button pressed");
+            }
+        });
+        
+        //Scene creation with grid as the root node
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        
+        //Initialize the css
+        scene.getStylesheets().add(LoginCSS.class.getResource("Login.css").toExternalForm());
+        
+        primaryStage.show();
+    }
+}
