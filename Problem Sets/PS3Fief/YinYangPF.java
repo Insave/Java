47ac7d442c976781c4e6f770525994e4f85e414b
Patrick Fief <froststang@gmail.com>
2018-04-06 21:18:20 -0500

    Chinese taijitu symbol: Yin Yang creator with random coloration

---
+++ Problem Sets/PS3Fief/YinYangPF.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+import javafx.scene.Group;
+ 
+public class YinYangPF extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        final double size = 400;
+        
+        BorderPane root = new BorderPane();
+        Group YinYang = new Group();
+        Scene scene = new Scene(root, size+25, size+25);
+
+        final Canvas canvas = new Canvas(size,size);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        //Creating 2 random colors
+        Color upperColor = new Color(getRandom(), getRandom(), getRandom(), 1.0);
+        Color lowerColor = new Color(getRandom(), getRandom(), getRandom(), 1.0);
+        
+        gc.setFill(upperColor);
+        // Use fillOval to draw a filled in circle
+        gc.fillOval(0, 0, size-1, size-1);
+ 
+        gc.setFill(lowerColor);
+        // Use fillArc to draw part of a filled in circle
+        gc.fillArc(0, 0, size-1, size-1, 270, 180, ArcType.ROUND);
+        gc.fillOval(size/4, size/2, size/2, size/2);
+ 
+        gc.setFill(upperColor);
+        gc.fillOval(size/4, 0, size/2, size/2);
+        gc.fillOval(7*size/16, 11*size/16, size/8, size/8);
+ 
+        gc.setFill(lowerColor);
+        gc.fillOval(7*size/16, 3*size/16, size/8, size/8);
+        // Use strokeOval to draw an empty circle for the outside border
+        gc.strokeOval(0, 0, size-1, size-1);
+        
+        YinYang.getChildren().add(canvas);
+        root.setCenter(YinYang);
+        
+        //Scene creation with root
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Yin and Yang");
+        primaryStage.show();
+    }
+    
+    private float getRandom() {
+        Random rand = new Random();
+        return rand.nextFloat();
+    }
+}
