b005643b73d838ab6aa9b084b04dfba03f344e73
Patrick Fief <froststang@gmail.com>
2018-04-07 17:18:02 -0500

    Finished EmotionalHearts

--- Problem Sets/PS3Fief/EmotionalHeartPF.java
+++ Problem Sets/PS3Fief/EmotionalHeartPF.java
@@ -31,28 +31,29 @@ public class EmotionalHeartPF extends Application {
     public void start(Stage primaryStage) {
         final int size = 500;
         
-        BorderPane root = new BorderPane();
+        GridPane root = new GridPane();
         Scene scene = new Scene(root, size, size);
         VBox btnBox = new VBox(10);
         btnBox.setAlignment(Pos.CENTER);
         btnBox.setMinWidth(size / 5);
         
+        final Canvas canvas = new Canvas(size * .75,size * .75);
+        
         HBox holderText = new HBox();
         holderText.setPrefHeight(size / 5);
-        holderText.setAlignment(Pos.TOP_CENTER);
+        holderText.setAlignment(Pos.CENTER);
         
         Text emotionText = new Text("Click a button");
         emotionText.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         emotionText.setTextAlignment(TextAlignment.CENTER);
         holderText.getChildren().addAll(emotionText);
         
-        createButtons(btnBox, emotionText);
+        createButtons(btnBox, emotionText, canvas);
         
         //TODO: Draw a Heart
-        
-        root.setLeft(btnBox);
-        BorderPane.setAlignment(holderText, Pos.CENTER);
-        root.setBottom(holderText);
+        root.add(canvas, 2, 0);
+        root.add(btnBox, 0, 0);
+        root.add(holderText, 2, 2);
         
         //Scene creation with root
         primaryStage.setScene(scene);
@@ -60,59 +61,75 @@ public class EmotionalHeartPF extends Application {
         primaryStage.show();
     }
     
-    //TODO: Remove color change, because gross black text
-    //TODO: After drawing the heart figure out how to swap its color
-    private void createButtons(Pane container, Text text) {
+    private void createButtons(Pane container, Text text, Canvas canvas) {
         Button red = new Button("Red");
         red.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.RED);
+            drawHeart(canvas, Color.RED);
             text.setText("Love & Anger");
-            //TODO Draw Heart
         });
         
         Button orange = new Button("Orange");
         orange.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.ORANGE);
+            drawHeart(canvas, Color.ORANGE);
             text.setText("Pride & Shame");
-            //TODO Draw Heart
         });
         
         Button yellow = new Button("Yellow");
         yellow.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.YELLOW);
+            drawHeart(canvas, Color.YELLOW);
             text.setText("Joy & Grief");
-            //TODO Draw Heart
         });
         
         Button green = new Button("Green");
         green.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.GREEN);
+            drawHeart(canvas, Color.GREEN);
             text.setText("Trust & Fear");
-            //TODO Draw Heart
         });
         
         Button blue = new Button("Blue");
         blue.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.BLUE);
+            drawHeart(canvas, Color.BLUE);
             text.setText("Peace & Hostility");
-            //TODO Draw Heart
         });
         
         Button indigo = new Button("Indigo");
         indigo.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.INDIGO);
+            drawHeart(canvas, Color.rgb(75, 0, 130));
             text.setText("Admiration & Disgust");
-            //TODO Draw Heart
         });
         
         Button violet = new Button("Violet");
         violet.setOnAction((ActionEvent e) -> {
-            text.setFill(Color.VIOLET);
+            drawHeart(canvas, Color.rgb(128, 0, 128));
             text.setText("Surprise & Boredom");
-            //TODO Draw Heart
         });
         
         container.getChildren().addAll(red, orange, yellow, green, blue, indigo, violet);
+    }
+    
+    //Possible to create QuadCurve objects and change their fill colors
+    private void drawHeart(Canvas canvas, Color color) {
+        final double leftMulti = .06;
+        final double rightMulti = .94;
+        final double heightDivisor = 3;
+        
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, 500, 500);
+        
+        gc.setFill(color);
+        gc.setStroke(color);
+        
+        gc.beginPath();
+        gc.moveTo(canvas.getWidth() * leftMulti, canvas.getHeight()/ heightDivisor -1);
+        //Upper
+        gc.quadraticCurveTo(canvas.getWidth() * leftMulti, (canvas.getHeight()/ heightDivisor -1) -150, canvas.getWidth()/2, canvas.getHeight()/ heightDivisor -1);
+        gc.quadraticCurveTo(canvas.getWidth() * rightMulti, (canvas.getHeight()/ heightDivisor -1) -150, canvas.getWidth() * rightMulti, canvas.getHeight()/ heightDivisor -1);
+        //Lower
+        gc.quadraticCurveTo(canvas.getWidth() * rightMulti -15, (canvas.getHeight()/ heightDivisor * 2 -1), canvas.getWidth()/2, canvas.getHeight() *rightMulti -1);
+        gc.quadraticCurveTo(canvas.getWidth() * leftMulti +15, (canvas.getHeight()/ heightDivisor * 2 -1), canvas.getWidth() * leftMulti, canvas.getHeight()/ heightDivisor -1);
+        
+        gc.stroke();
+        gc.fill();
     }
     
 }
